db8648ee80a41b424e4a37a76550893e747c4774
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-19 17:49:40 -0400

    v0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '0.3.0'
+version          '0.3.1'
 
 depends 'chef-server'
 depends 'chef-analytics'

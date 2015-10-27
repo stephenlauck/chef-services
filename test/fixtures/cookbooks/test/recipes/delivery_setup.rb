7515e52fdb787c84954e7eb86ba35c1e0ccc17c3
Stephen Lauck <stephen.lauck@gmail.com>
2015-10-27 17:28:15 -0400

    upload all needed cookbooks to chef server

--- test/fixtures/cookbooks/test/recipes/delivery_setup.rb
+++ test/fixtures/cookbooks/test/recipes/delivery_setup.rb
@@ -18,3 +18,22 @@ chef_server_org 'chef_delivery' do
   admins %w{ delivery }
   action :add_admin
 end
+
+file '/tmp/config.rb' do
+  content <<-EOF
+log_level                :info
+log_location             STDOUT
+node_name                "delivery"
+client_key               "/tmp/delivery.pem"
+validation_client_name   "chef_delivery-validator"
+validation_key           "/tmp/delivery-validator.pem"
+chef_server_url          "https://chef.example.com/organizations/chef_delivery"
+cookbook_path            ["/tmp/kitchen/cookbooks"]
+EOF
+end
+
+execute 'knife ssl fetch -c /tmp/config.rb'
+
+execute 'knife upload cookbooks data_bags --chef-repo-path . -c ../config.rb' do
+  cwd '/tmp/kitchen'
+end

5c58df5518de46b951d6dcec4d17a6ea2be5affb
Stephen Lauck <stephen.lauck@gmail.com>
2015-10-27 19:26:50 -0400

    break out delivery license config code to recipe

---
+++ test/fixtures/cookbooks/test/recipes/delivery_license.rb
@@ -0,0 +1,2 @@
+# scp -i ~/.vagrant.d/insecure_private_key /Users/stephen/delivery.license root@33.33.33.13:/var/opt/delivery/license/delivery.license
+execute 'cat /var/opt/delivery/license/delivery.license'

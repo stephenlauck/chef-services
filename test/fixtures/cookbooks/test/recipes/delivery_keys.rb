9e5f2280eeab03401bfbd268f586f61ad8f1bfa5
Stephen Lauck <stephen.lauck@gmail.com>
2015-10-27 17:27:21 -0400

    add dir for delivery.license
    check for license and fail if not found

--- test/fixtures/cookbooks/test/recipes/delivery_keys.rb
+++ test/fixtures/cookbooks/test/recipes/delivery_keys.rb
@@ -5,6 +5,10 @@ directory '/etc/delivery' do
   recursive true
 end
 
+directory '/var/opt/delivery/license/' do
+  recursive true
+end
+
 execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r root@chef.example.com:/tmp/delivery.pem /etc/delivery/delivery.pem"
 
 cookbook_file '/etc/delivery/builder_key' do
@@ -22,3 +26,6 @@ cookbook_file '/etc/delivery/builder_key.pub' do
   mode '0600'
   source 'insecure_public_key'
 end
+
+# scp -i ~/.vagrant.d/insecure_private_key /Users/stephen/delivery.license root@33.33.33.13:/var/opt/delivery/license/delivery.license
+execute 'cat /var/opt/delivery/license/delivery.license'

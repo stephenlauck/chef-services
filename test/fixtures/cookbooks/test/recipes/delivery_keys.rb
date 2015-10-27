544553d54cb1096502e4165f66529996ad874703
Stephen Lauck <stephen.lauck@gmail.com>
2015-10-27 19:28:52 -0400

    move delivery.license to own recipe and remove from delivery_keys

--- test/fixtures/cookbooks/test/recipes/delivery_keys.rb
+++ test/fixtures/cookbooks/test/recipes/delivery_keys.rb
@@ -26,6 +26,3 @@ cookbook_file '/etc/delivery/builder_key.pub' do
   mode '0600'
   source 'insecure_public_key'
 end
-
-# scp -i ~/.vagrant.d/insecure_private_key /Users/stephen/delivery.license root@33.33.33.13:/var/opt/delivery/license/delivery.license
-execute 'cat /var/opt/delivery/license/delivery.license'

660c21fb0d51b5afe276ba5e916cc5c52a98690e
Stephen Lauck <stephen.lauck@gmail.com>
2015-10-29 17:19:48 -0400

    build supermarket via custom resource

--- test/fixtures/cookbooks/test/recipes/supermarket_oc_id.rb
+++ test/fixtures/cookbooks/test/recipes/supermarket_oc_id.rb
@@ -1,20 +1,10 @@
 
 execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r root@chef.example.com:/etc/opscode/oc-id-applications/supermarket.json /tmp/"
 
-template '/etc/supermarket/supermarket.json' do
-  source 'supermarket.json.erb'
-  owner 'root'
-  group 'root'
-  variables(
-    lazy {
-      {
-        :chef_server_url => node['supermarket_omnibus']['chef_server_url'],
-        :chef_oauth2_app_id => get_supermarket_attribute('uid'),
-        :chef_oauth2_secret => get_supermarket_attribute('secret'),
-        :chef_oauth2_verify_ssl => node['supermarket_omnibus']['chef_oauth2_verify_ssl']
-      }
-    }
-  )
-  action :create
-  notifies :reconfigure, 'chef_server_ingredient[supermarket]'
+supermarket_server 'supermarket' do
+  chef_server_url node['supermarket_omnibus']['chef_server_url']
+  chef_oauth2_app_id get_supermarket_attribute('uid')
+  chef_oauth2_secret get_supermarket_attribute('secret')
+  chef_oauth2_verify_ssl node['supermarket_omnibus']['chef_oauth2_verify_ssl']
+  config node['supermarket_omnibus']['config'].to_hash
 end

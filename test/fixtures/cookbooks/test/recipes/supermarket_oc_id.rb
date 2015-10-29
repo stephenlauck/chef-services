661d902c4c5683570d43d1621c0de98740fca9cd
Stephen Lauck <stephen.lauck@gmail.com>
2015-10-29 17:34:14 -0400

    correct redirect_urls lazy load supermarket attributes

--- test/fixtures/cookbooks/test/recipes/supermarket_oc_id.rb
+++ test/fixtures/cookbooks/test/recipes/supermarket_oc_id.rb
@@ -3,8 +3,8 @@ execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r r
 
 supermarket_server 'supermarket' do
   chef_server_url node['supermarket_omnibus']['chef_server_url']
-  chef_oauth2_app_id get_supermarket_attribute('uid')
-  chef_oauth2_secret get_supermarket_attribute('secret')
+  chef_oauth2_app_id lazy { get_supermarket_attribute('uid') }
+  chef_oauth2_secret lazy { get_supermarket_attribute('secret') }
   chef_oauth2_verify_ssl node['supermarket_omnibus']['chef_oauth2_verify_ssl']
   config node['supermarket_omnibus']['config'].to_hash
 end

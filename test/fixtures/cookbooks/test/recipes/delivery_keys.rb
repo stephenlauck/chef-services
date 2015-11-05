737e5151766b9aae72ba1af77d6ca7129315f588
Stephen Lauck <stephen.lauck@gmail.com>
2015-11-05 13:10:47 -0500

    add 45 day trial delivery license

--- test/fixtures/cookbooks/test/recipes/delivery_keys.rb
+++ test/fixtures/cookbooks/test/recipes/delivery_keys.rb
@@ -22,3 +22,11 @@ cookbook_file '/etc/delivery/builder_key.pub' do
   mode '0600'
   source 'insecure_public_key'
 end
+
+directory '/var/opt/delivery/license' do
+  recursive true
+end
+
+cookbook_file '/var/opt/delivery/license/delivery.license' do
+  source 'delivery.license'
+end

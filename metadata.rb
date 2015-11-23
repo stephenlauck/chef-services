5103461a29d3025144ce6c714531e2ba006304f1
Stephen Lauck <lauck@chef.io>
2015-11-23 13:56:28 -0500

    bump metadata to 1.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '1.2.0'
+version          '1.3.0'
 
 depends 'chef-server'
 depends 'chef-analytics'

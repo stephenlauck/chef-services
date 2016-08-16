6f0c23c7b2c041e79d20760aae0b3c2d6667e90f
Stephen Lauck <lauck@chef.io>
2016-08-16 11:58:04 -0400

    add inspec testing

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '2.0.0'
+version          '2.0.1'
 
 depends 'yum'
 depends 'apt'

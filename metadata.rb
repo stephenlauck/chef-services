fd4e83a24362626ecc73aa5f7291306ea87532f5
Stephen Lauck <lauck@chef.io>
2016-08-25 09:44:54 -0400

    remove suse

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '2.0.4'
+version          '2.0.5'
 
 depends 'yum'
 depends 'apt'
--- test/fixtures/cookbooks/test/recipes/suse_connect.rb
+++
@@ -1,1 +0,0 @@
-execute 'SUSEConnect -r DE98C0808F7DD8 -e andy.k.dufour@gmail.com'

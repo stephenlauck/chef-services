baae961a7be71fb47fb86687d2e6890be57a5c67
Stephen Lauck <lauck@chef.io>
2016-08-25 09:42:50 -0400

    remove monkey patch

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '2.0.3'
+version          '2.0.4'
 
 depends 'yum'
 depends 'apt'

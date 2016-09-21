867998931f295e90877dc620adcb226d4da6e5eb
andy-dufour <adufour@chef.io>
2016-09-21 16:05:30 -0400

    Updating installer script

--- files/default/installer.sh
+++ files/default/installer.sh
@@ -12,28 +12,30 @@
 # ---------- Chef Server ----------
 # ->install Chef
 
-curl -L https://www.chef.io/chef/install.sh -P chefdk | sudo bash
+#curl -LO https://omnitruck.chef.io/install.sh && sudo bash ./install.sh -P chefdk && rm install.sh
+
 echo 'cookbook_path \"/tmp/workspace/cookbooks\"' > /tmp/solo.rb
 sudo chef-solo -c /tmp/solo.rb -o 'recipe[chef_server::default]'
 
-cat > /tmp/workspace/Berksfile <<EOF
+mkdir /tmp/chef_installer
+yum install git -y
+cat > /tmp/chef_installer/Berksfile <<EOF
 source 'https://supermarket.chef.io'
 
 cookbook 'chef-server-ctl', git: 'https://github.com/stephenlauck/chef-server-ctl.git'
-cookbook 'chef_server', path: 'cookbooks/chef_server'
-cookbook 'chef_automate', path: 'cookbooks/chef_automate'
+cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'installer'
+cookbook 'chef-ingredient', git: 'https://github.com/chef-cookbooks/chef-ingredient.git'
+
 EOF
 
-cd /tmp/workspace/
+cd /tmp/chef_installer/
 berks install
 berks vendor cookbooks/
 
-
-
-
-
 # ->scp/upload cookbook tarball
 # ->chef-client -z with Chef server cookbook (from tarball ^^)
+chef-client -z -r "test::chef-server"
+
 # ->upload cookbooks to itself
 # ->generate keys, create data_bags
 # ->bootstrap Chef to Chef

cd7a005ff416141c1e457f35b66fcd3281d21cd6
Stephen Lauck <lauck@chef.io>
2016-09-22 16:31:33 -0400

    drop delivery.license from cookbook add delivery license to data bag

--- test/fixtures/cookbooks/test/recipes/delivery_license.rb
+++ test/fixtures/cookbooks/test/recipes/delivery_license.rb
@@ -1,6 +1,7 @@
+directory '/var/opt/delivery/license' do
+  recursive true
+end
+
 cookbook_file '/var/opt/delivery/license/delivery.license' do
   source 'delivery.license'
-  owner 'root'
-  group 'root'
-  mode 00644
 end

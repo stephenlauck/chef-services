1e9385948f7f1aede19e1818d223dd092f963ae6
Stephen Lauck <lauck@chef.io>
2016-09-22 14:17:13 -0400

    add berks update

--- files/default/installer.rb
+++ files/default/installer.rb
@@ -10,11 +10,17 @@ package 'git'
 
 file '/tmp/chef_installer/Berksfile' do
   content "source 'https://supermarket.chef.io'
-  
+
 cookbook 'chef-server-ctl', git: 'https://github.com/stephenlauck/chef-server-ctl.git'
-cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'installer'
+cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'lauck/delivery_license'
 cookbook 'chef-ingredient', git: 'https://github.com/chef-cookbooks/chef-ingredient.git'
 cookbook 'test', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'installer', rel: 'test/fixtures/cookbooks/test'"
+end
+
+execute 'berks update' do
+  command 'berks update'
+  cwd '/tmp/chef_installer'
+  only_if File.exists?('/tmp/chef_installer/Berksfile.lock')
 end
 
 execute 'berks install' do

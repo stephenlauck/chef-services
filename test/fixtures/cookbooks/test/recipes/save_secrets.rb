ec8608e23d2b35f79f2fd7c641aeb2797be38845
andy-dufour <adufour@chef.io>
2016-09-21 23:34:58 -0400

    Adding save_secrets.rb

---
+++ test/fixtures/cookbooks/test/recipes/save_secrets.rb
@@ -0,0 +1,49 @@
+#TODO: Replace with chef vault or similar. Non-encrypted databags == bad.
+require 'json'
+require 'net/ssh'
+
+chef_ingredient 'chefdk'
+
+package 'git'
+
+directory '/tmp/chef_installer/.chef/' do
+  action :create
+  recursive true
+end
+
+file '/tmp/chef_installer/.chef/knife.rb' do
+  content '
+node_name                "delivery"
+client_key               "/tmp/delivery.pem"
+chef_server_url          "https://localhost/organizations/delivery"
+cookbook_path            ["/tmp/chef_installer/cookbooks"]
+ssl_verify_mode          :verify_none
+'
+end
+
+builder_key = OpenSSL::PKey::RSA.new(2048)
+
+directory '/tmp/chef_installer/data_bags'
+
+ruby_block 'write_automate_databag' do
+  block do
+    automate_db_item = {
+      'id' => 'automate',
+      'validator_pem' => ::File.read('/tmp/delivery-validator.pem'),
+      'user_pem' => ::File.read('/tmp/delivery.pem'),
+      'builder_pem' => builder_key.to_pem,
+      'builder_pub' => "ssh-rsa #{[builder_key.to_blob].pack('m0')}"
+    }
+    ::File.write('/tmp/chef_installer/data_bags/automate.json', automate_db_item.to_json)
+  end
+end
+
+execute 'upload databag' do
+  command 'knife data bag create automate;knife data bag from file automate data_bags/automate.json'
+  cwd '/tmp/chef_installer'
+end
+
+execute 'upload cookbooks' do
+  command 'berks install;berks upload --no-ssl-verify'
+  cwd '/tmp/chef_installer'
+end

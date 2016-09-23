d3e489f28760dbb2f34a5a7527588d01ef5695b9
andy-dufour <adufour@chef.io>
2016-09-23 14:30:08 -0400

    simplifying installer

--- files/default/installer.sh
+++ files/default/installer.sh
@@ -7,16 +7,14 @@ read -e -p "We are about to install Chef server on this system, proceed? (y/n) [
 name=${name:-n}
 
 read -e -p "Please provide an IP/FQDN for your chef server: " -i "chef.services.com" chef_server_fqdn
-read -e -p "Do you want to bootstrap your Delivery server with knife? (y/n) [n]" -i "y" bootstrap_more
+read -e -p "Do you want to bootstrap your Automate server now? (y/n) [n]" -i "y" bootstrap_more
 bootstrap_more=${bootstrap_more:-n}
 
 if [ "$bootstrap_more" == "y" ]; then
   read -e -p "Please provide an IP/FQDN for your Automate server: " -i "automate.services.com" chef_automate_fqdn
-  read -e -p "What's the SSH username for your Automate server: " -i "vagrant" chef_automate_user
-  read -e -p "What's the SSH password for your Automate server: " -i "vagrant" chef_automate_pw
   read -e -p "Please provide an IP/FQDN for your Build node: " -i "build.services.com" chef_build_fqdn
-  read -e -p "What's the SSH username for your Build node: " -i "vagrant" chef_build_user
-  read -e -p "What's the SSH password for your Build node: " -i "vagrant" chef_build_pw
+  read -e -p "What's the SSH username for your servers: " -i "vagrant" chef_user
+  read -e -p "What's the SSH password for your servers: " -i "vagrant" chef_pw
 fi
 
 # Do you want to connect by ssh key or user/pass?
@@ -45,8 +43,11 @@ chef-client -z -j attributes.json -r 'recipe[test::hostsfile],recipe[test::chef-
 # --> bootstrap with correct runlist
 
 if [ "$bootstrap_more" == "y" ]; then
-  knife bootstrap $chef_build_fqdn -N $chef_build_fqdn -x $chef_build_user -P $chef_build_pw --sudo -r "recipe[test::hostsfile]" -y --node-ssl-verify-mode none
-  knife bootstrap $chef_automate_fqdn -N $chef_automate_fqdn -x $chef_automate_user -P $chef_automate_pw --sudo -r "recipe[hostsfile],recipe[chef_automate]" -j "{\"chef_automate\":{\"fqdn\":\"$chef_automate_fqdn\",\"build_nodes\":[{\"fqdn\":\"$chef_build_fqdn\",\"username\":\"$chef_build_user\",\"password:\":\"$chef_build_pw\"}]}}" -y --node-ssl-verify-mode none
+  knife bootstrap $chef_build_fqdn -N $chef_build_fqdn -x $chef_user -P $chef_pw --sudo -r "recipe[test::hostsfile]" -y --node-ssl-verify-mode none
+  knife bootstrap $chef_automate_fqdn -N $chef_automate_fqdn -x $chef_user -P $chef_pw \
+    --sudo -r "recipe[hostsfile],recipe[chef_automate]" \
+    -j "{\"chef_automate\":{\"fqdn\":\"$chef_automate_fqdn\",\"build_nodes\":[{\"fqdn\":\"$chef_build_fqdn\",\"username\":\"$chef_user\",\"password:\":\"$chef_pw\"}]}}" \
+    -y --node-ssl-verify-mode none
 fi
 
 chef-client -j attributes.json -r 'recipe[test::chef-server]'

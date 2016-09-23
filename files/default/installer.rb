e071080c2191294d51afbe678a0d0cd02b7e07d0
andy-dufour <adufour@chef.io>
2016-09-23 13:59:09 -0400

    Updating Berksfile

--- files/default/installer.rb
+++ files/default/installer.rb
@@ -12,9 +12,9 @@ file '/tmp/chef_installer/Berksfile' do
   content "source 'https://supermarket.chef.io'
 
 cookbook 'chef-server-ctl', git: 'https://github.com/stephenlauck/chef-server-ctl.git'
-cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'lauck/delivery_license'
+cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'ad/fixes'
 cookbook 'chef-ingredient', git: 'https://github.com/chef-cookbooks/chef-ingredient.git'
-cookbook 'test', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'lauck/delivery_license', rel: 'test/fixtures/cookbooks/test'"
+cookbook 'test', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'ad/fixes', rel: 'test/fixtures/cookbooks/test'"
 end
 
 execute 'berks update' do

1571039d066919edf15c0b6c48dcf6efeb65fb0f
andy-dufour <adufour@chef.io>
2016-09-29 00:07:16 -0400

    Adding working recipes for TK

--- test/fixtures/cookbooks/test/recipes/save_secrets.rb
+++ test/fixtures/cookbooks/test/recipes/save_secrets.rb
@@ -46,7 +46,16 @@ execute 'upload databag' do
   cwd "#{node['chef_server']['install_dir']}/chef_installer"
 end
 
+file "#{node['chef_server']['install_dir']}/chef_installer/Berksfile" do
+  content "source 'https://supermarket.chef.io'
+
+cookbook 'chef-server-ctl', git: 'https://github.com/stephenlauck/chef-server-ctl.git'
+cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'ad/fixes'
+cookbook 'chef-ingredient', git: 'https://github.com/chef-cookbooks/chef-ingredient.git'
+cookbook 'test', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'ad/fixes', rel: 'test/fixtures/cookbooks/test'"
+end
+
 execute 'upload cookbooks' do
-  command 'berks install;berks upload --no-ssl-verify'
+  command 'berks install;berks upload --no-ssl-verify --force'
   cwd "#{node['chef_server']['install_dir']}/chef_installer"
 end

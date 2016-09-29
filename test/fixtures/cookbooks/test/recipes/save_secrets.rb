76ab148442e6a43b48aa3e1d69a30dc39e68d9ec
andy-dufour <adufour@chef.io>
2016-09-29 00:35:45 -0400

    Updating branch

--- test/fixtures/cookbooks/test/recipes/save_secrets.rb
+++ test/fixtures/cookbooks/test/recipes/save_secrets.rb
@@ -50,9 +50,9 @@ file "#{node['chef_server']['install_dir']}/chef_installer/Berksfile" do
   content "source 'https://supermarket.chef.io'
 
 cookbook 'chef-server-ctl', git: 'https://github.com/stephenlauck/chef-server-ctl.git'
-cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'ad/fixes'
+cookbook 'chef-services', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'installer'
 cookbook 'chef-ingredient', git: 'https://github.com/chef-cookbooks/chef-ingredient.git'
-cookbook 'test', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'ad/fixes', rel: 'test/fixtures/cookbooks/test'"
+cookbook 'test', git: 'https://github.com/stephenlauck/chef-services.git', branch: 'installer', rel: 'test/fixtures/cookbooks/test'"
 end
 
 execute 'upload cookbooks' do

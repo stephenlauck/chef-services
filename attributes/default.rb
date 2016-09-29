293d0c47f9ecc21b559d3af1084bf1e21e0ee7cb
andy-dufour <adufour@chef.io>
2016-09-29 12:51:15 -0400

    Fixing build node

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,5 @@ default['compliance']['channel'] = :stable
 default['compliance']['accept_license'] = false
 default['chef_automate']['fqdn'] = 'automate.services.com'
 default['chefdk']['bashrc'] = '/etc/bashrc'
+default['chefdk']['version'] = 'latest'
+default['chefdk']['source'] = "http://omnitruck.chef.io/stable/chefdk/metadata?p=#{node['platform']}&pv=#{node['platform_version']}&m=#{node['kernel']['processor']}&v=#{node['chef-services']['chefdk_version']}"
--- recipes/install_build_nodes.rb
+++ recipes/install_build_nodes.rb
@@ -1,3 +1,13 @@
+case node['platform']
+when 'ubuntu'
+  chefdk_ext = 'deb'
+else
+  chefdk_ext = 'rpm'
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/chefdk.#{chefdk_ext}" do
+  source node['chefdk']['source']
+end
 
 file '/etc/chef/validation.pem' do
   action :delete
@@ -5,7 +15,7 @@ end
 
 node['chef_automate']['build_nodes'].each do |build_node|
   execute "Install build node #{build_node['fqdn']}" do
-    command "delivery-ctl install-build-node --fqdn #{build_node['fqdn']} --username #{build_node['username']} --password #{build_node['password']} --installer /tmp/chefdk.el6.x86_64.rpm --overwrite-registration"
+    command "delivery-ctl install-build-node --fqdn #{build_node['fqdn']} --username #{build_node['username']} --password #{build_node['password']} --installer #{Chef::Config[:file_cache_path]}/chefdk.#{chefdk_ext} --overwrite-registration"
     action :run
   end
 end

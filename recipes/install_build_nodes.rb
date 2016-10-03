70a7df87a7410cd9cced31658e7cb1ee73b68647
andy-dufour <adufour@chef.io>
2016-10-03 16:36:38 -0400

    Fixing ssl cert fetching

--- recipes/install_build_nodes.rb
+++ recipes/install_build_nodes.rb
@@ -10,7 +10,10 @@ end
 
 %W(#{node['chef_server']['fqdn']} #{node['chef_automate']['fqdn']}).each do |server|
   execute "fetch ssl cert for #{server}" do
-    command "knife ssl fetch https://#{server} -c /etc/chef/client.rb"
+    command "knife ssl fetch -s https://#{server}"
+  end
+  execute "fetch ssl cert for #{server}" do
+    command "knife ssl fetch -s https://#{server} -c /etc/chef/client.rb"
   end
 end
 

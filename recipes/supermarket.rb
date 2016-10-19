47c8486ab71d59c67c2da96ff743b8eb6d77cce3
andy-dufour <adufour@chef.io>
2016-10-19 15:50:36 -0400

    Fixing secret attribute

--- recipes/supermarket.rb
+++ recipes/supermarket.rb
@@ -6,7 +6,7 @@ delivery_databag = data_bag_item('automate', 'automate')
 supermarket_server 'supermarket' do
   chef_server_url "https://#{node['chef_server']['fqdn']}"
   chef_oauth2_app_id delivery_databag['supermarket_oauth2_app_id']
-  chef_oauth2_secret delivery_databag['supermarket_oauth2_app_id']
+  chef_oauth2_secret delivery_databag['supermarket_oauth2_secret']
   chef_oauth2_verify_ssl node['supermarket_omnibus']['chef_oauth2_verify_ssl']
   config node['supermarket_omnibus']['config'].to_hash
 end

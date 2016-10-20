56bf9f2fb56e235e49277735dda966430121e057
andy-dufour <adufour@chef.io>
2016-10-20 12:23:20 -0400

    Correcting compliance reconfigure

--- recipes/compliance.rb
+++ recipes/compliance.rb
@@ -5,16 +5,13 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{file_info['name']}" do
   not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/#{file_info['name']}") }
 end
 
-chef_ingredient 'compliance' do
-	package_source "#{Chef::Config[:file_cache_path]}/#{file_info['name']}"
-	accept_license node['chef-services']['compliance']['accept_license'] unless node['compliance']['accept_license'].nil?
-end
-
 user 'chef-compliance' do
   group 'chef-compliance'
   action :manage
 end
 
-ingredient_config 'compliance' do
-  notifies :reconfigure, 'chef_ingredient[compliance]', :immediately
+chef_ingredient 'compliance' do
+	package_source "#{Chef::Config[:file_cache_path]}/#{file_info['name']}"
+	accept_license node['chef-services']['compliance']['accept_license'] unless node['compliance']['accept_license'].nil?
+  action [:install,:reconfigure]
 end

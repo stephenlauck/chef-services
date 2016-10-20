2605b6974682279f3ca4f5f1ef3f0e6aa33d9ed7
andy-dufour <adufour@chef.io>
2016-10-20 12:52:27 -0400

    Adding compliance node test, adding SuSE specific compliance fixes

--- recipes/compliance.rb
+++ recipes/compliance.rb
@@ -5,13 +5,47 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{file_info['name']}" do
   not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/#{file_info['name']}") }
 end
 
-user 'chef-compliance' do
-  group 'chef-compliance'
-  action :manage
-end
-
 chef_ingredient 'compliance' do
 	package_source "#{Chef::Config[:file_cache_path]}/#{file_info['name']}"
 	accept_license node['chef-services']['compliance']['accept_license'] unless node['compliance']['accept_license'].nil?
   action [:install,:reconfigure]
+end
+
+# Extra steps needed for SuSE
+
+if node['platform'] == 'suse'
+
+  user 'chef-compliance' do
+    group 'chef-compliance'
+    action :manage
+  end
+
+  user 'chef-pgsql' do
+    group 'chef-compliance'
+    action :manage
+  end
+
+  user 'dex-worker' do
+    group 'dex-worker'
+    action :manage
+  end
+
+  user 'dex-overlord' do
+    group 'dex-overlord'
+    action :manage
+  end
+
+  file '/etc/chef-compliance/server-config.json' do
+    owner 'chef-compliance'
+  end
+
+  execute 'chown core' do
+    command 'chown -R chef-compliance.chef-compliance /var/opt/chef-compliance/core'
+    action :run
+  end
+
+  execute 'restart chef-compliance' do
+    command 'chef-compliance-ctl restart'
+  end
+
 end

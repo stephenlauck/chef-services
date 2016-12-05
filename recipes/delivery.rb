64e0c7fabb7e2f17aec90dbfa3f722b6006f534a
Nathan Cerny <ncerny@gmail.com>
2016-12-05 17:05:53 -0600

    Convert recipes to use resources for deploying Chef Software.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- recipes/delivery.rb
+++ recipes/delivery.rb
@@ -4,42 +4,15 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-directory '/var/opt/delivery/license/' do
-  recursive true
-end
-
-directory '/etc/delivery'
-directory '/etc/chef'
-
 delivery_databag = data_bag_item('automate', 'automate')
 
-include_recipe 'chef-services::delivery_license'
-
-file '/etc/delivery/delivery.pem' do
-  content delivery_databag['user_pem']
+chef_automate 'automate.services.com' do
+  chef_user 'delivery'
+  chef_user_pem delivery_databag['user_pem']
+  validation_pem delivery_databag['validator_pem']
+  builder_pem delivery_databag['builder_pem']
+  config node['delivery']['config']
+  enterprise 'test'
+  license 'cookbook_file://chef-services::delivery.license'
+  accept_license node['chef-services']['accept_license']
 end
-
-file '/etc/chef/validation.pem' do
-  content delivery_databag['validator_pem']
-end
-
-%w( delivery chefdk ).each do |svc|
-  remote_file "#{svc} package" do
-    path "#{node['chef_server']['install_dir']}/#{::File.basename(node[svc]['package_url'])}"
-    source node[svc]['package_url']
-    only_if { node[svc]['package_url'] }
-  end
-
-  chef_ingredient svc do
-    config node[svc]['config'] if node[svc]['config']
-    package_source "#{node['chef_server']['install_dir']}/#{::File.basename(node[svc]['package_url'])}" if node[svc]['package_url']
-    accept_license node['chef-services']['accept_license']
-    action :upgrade
-  end
-
-  ingredient_config svc do
-    notifies :reconfigure, "chef_ingredient[#{svc}]", :immediately
-  end
-end
-
-include_recipe 'chef-services::create_enterprise'
--- recipes/supermarket.rb
+++ recipes/supermarket.rb
@@ -1,34 +1,15 @@
-
-remote_file "supermarket package" do
-  path "#{node['chef_server']['install_dir']}/#{::File.basename(node['supermarket']['package_url'])}"
-  source node['supermarket']['package_url']
-  only_if { node['supermarket']['package_url'] }
-end
-
 delivery_databag = data_bag_item('automate', 'automate')
 
-supermarket_config = {
-      'chef_server_url' => "https://#{node['chef_server']['fqdn']}",
-      'chef_oauth2_app_id' => delivery_databag['supermarket_oauth2_app_id'],
-      'chef_oauth2_secret' => delivery_databag['supermarket_oauth2_secret'],
-      'chef_oauth2_verify_ssl' => node['chef-supermarket']['supermarket']['verify_ssl']
-    }
-
-chef_ingredient 'supermarket' do
-  config JSON.pretty_generate(supermarket_config.merge(node['chef-services']['supermarket']['config']))
-  ctl_command '/opt/supermarket/bin/supermarket-ctl'
-  sensitive   true
-  package_source "#{node['chef_server']['install_dir']}/#{::File.basename(node['supermarket']['package_url'])}" if node['supermarket']['package_url']
+user 'supermarket' do
+  group 'supermarket'
+  only_if { node['platform'].eql?('suse') }
 end
 
-ingredient_config 'supermarket' do
-  notifies :reconfigure, 'chef_ingredient[supermarket]', :immediately
-end
-
-if node['platform'] == 'suse'
-  user 'supermarket' do
-    group 'supermarket'
-    action :manage
-    notifies :reconfigure, 'chef_ingredient[supermarket]', :immediately
-  end
+chef_supermarket 'supermarket.services.com' do
+  chef_server_url "https://#{node['chef_server']['fqdn']}"
+  chef_oauth2_app_id delivery_databag['supermarket_oauth2_app_id']
+  chef_oauth2_secret delivery_databag['supermarket_oauth2_secret']
+  chef_oauth2_verify_ssl node['chef-supermarket']['supermarket']['verify_ssl']
+  config node['chef-services']['supermarket']['config']
+  accept_license node['chef-services']['accept_license']
 end

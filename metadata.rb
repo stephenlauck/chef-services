74f2aa61a569fd4a5ffea333d117796cf9bb7af8
Stephen Lauck <lauck@chef.io>
2017-01-29 16:18:38 -0500

    bump to v4.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '4.0.3'
+version          '4.1.0'
 
 depends 'ntp'
 depends 'fancy_execute'

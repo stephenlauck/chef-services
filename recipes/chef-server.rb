8a11225e205a98a1f664997e94927f3ced2b8803
Stephen Lauck <lauck@chef.io>
2017-01-31 16:05:06 -0500

    remove data collector config from chef-server.rb until race conditions or guards can be implemented

--- recipes/chef-server.rb
+++ recipes/chef-server.rb
@@ -11,7 +11,7 @@ EOS
   addons manage: { version: '2.4.3', config: '' },
          :"push-jobs-server" => { version: '2.1.0', config: '' }
   accept_license true
-  data_collector_url 'https://automate.services.com/data-collector/v0/' if search(:node, 'name:automate-centos-68', filter_result: { 'name' => ['name'] }) # ~FC003
+  # data_collector_url 'https://automate.services.com/data-collector/v0/' if search(:node, 'name:automate-centos-68', filter_result: { 'name' => ['name'] }) # ~FC003
 end
 
 chef_user 'delivery' do

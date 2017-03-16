b39704efe769bf2821619030f3ab1f84f368dd81
Stephen Lauck <lauck@chef.io>
2017-03-16 13:00:28 -0400

    v5.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures chef-services'
 long_description 'Installs/Configures chef-services'
-version          '5.0.0'
+version          '5.1.0'
 
 depends 'fancy_execute'
 depends 'chef_stack'
